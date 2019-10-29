7143348520ca4e95abbdc4ab67d766665b6c1211
jihyelee <>
2019-10-29 18:51:05 +0900

    2nd

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const http = require('http')
 
-
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 
+
 app.get('/', (req, res) => {
     res.status(200).send('Server is working')
 })
+
 
 app.listen(port, () => {
     console.log(`Server is runnint at https://localhost:${port}`)
