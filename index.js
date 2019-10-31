c22d372e941f696b1bbdbc8e33979fa8ed340579
jihyelee <>
2019-10-31 17:14:36 +0900

    4th

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-const express = require('express')
-const http = require('http')
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-const app = express()
-const port = process.env.PORT || 3000
+app.use(express.json());
+app.use(express.urlencoded( {extended : false } )) //false: url 인코딩 안함
 
-app.use(express.json())
+const ejs = require("ejs");
+const path = require("path");
 
+app.set("view engine", "ejs") // app에 view eingine 설치, ejs를 템풀릿으로
+app.use(express.static(path.join(__dirname, '/'))) ;
+app.use(express.static('views'))
 
 app.get('/', (req, res) => {
-    res.status(200).send('Server is working')
+    console.log("main ejs");
+    res.render("main", {});
 })
-
 
 app.listen(port, () => {
     console.log(`Server is runnint at https://localhost:${port}`)
